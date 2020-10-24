40048ac0e5584f8e8ef92b4f226ed557969fa56b
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-24 17:52:02 +0900

    [ADD] wireFrame

--- src/components/GreenBox/GreenBox.tsx
+++ src/components/GreenBox/GreenBox.tsx
@@ -10,16 +10,21 @@ const GreenBox = () => {
       0.1,
       1000
     );
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var material = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true,
+    });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
     camera.position.z = 5;
     const animate = () => {
       requestAnimationFrame(animate);
+      console.log(cube.rotation.x);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
       renderer.render(scene, camera);
@@ -31,7 +36,7 @@ const GreenBox = () => {
     greenBoxFunc();
   });
 
-  return <></>;
+  return <div />;
 };
 
 export default GreenBox;
