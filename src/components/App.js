1b4f383697ed42dabbcafc85a9cbb22be13b1450
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-26 11:45:46 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Box from './Box';
+import Hexaindex from './Hexahedron/Hexaindex';
+import './App.scss';
 const App = () => {
   return (
     <>
       <Box />
+      <Hexaindex />
     </>
   );
 };
