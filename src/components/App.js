3d33554ab794ba6f8bf4f80ec9555aea637bfd85
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-10-27 19:20:01 +0900

    [ADD] materialBox Sky sh

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,15 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Box from './Box';
 import Hexaindex from './Hexahedron/Hexaindex';
 import './App.scss';
+import Drop from './Cannon/CannonIndex';
+import MaterialBoxView from './MaterialBox/MaterialBox';
 const App = () => {
   return (
     <>
       <Box />
       <Hexaindex />
+      <MaterialBoxView />
+      {/* <Drop /> */}
     </>
   );
 };
---
+++ src/components/MaterialBox/MaterialBox.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { Box, OrbitControls, Sky } from '@react-three/drei';
+
+const MetalBox = () => {
+  const [renderTarget] = useState(
+    new THREE.WebGLCubeRenderTarget(1024, {
+      format: THREE.RGBAFormat,
+      generateMipmaps: true,
+    })
+  );
+  const cubeCamera = useRef();
+
+  useFrame(({ gl, scene }) => {
+    cubeCamera.current.update(gl, scene);
+  });
+  console.log(renderTarget.texture);
+  console.log(renderTarget);
+  return (
+    <>
+      <Sky
+        layer={[11]}
+        sunPosition={[Math.PI, 0, Math.PI / 2]}
+        turbidity={8}
+        rayleigh={6}
+        mieCoefficient={0.005}
+        mieDirectionalG={0.8}
+      />
+      <cubeCamera
+        layer={[11]}
+        name="cubeCamera"
+        ref={cubeCamera}
+        position={[0, 0, 0]}
+        args={[0.1, 100, renderTarget]}
+      />
+      <Box args={[2, 2, 2]}>
+        <meshPhysicalMaterial
+          color="#17a2b8"
+          envMap={renderTarget.texture}
+          metalness={1}
+          roughness={0}
+          antialias={true}
+        />
+      </Box>
+      <OrbitControls />
+      <ambientLight />
+    </>
+  );
+};
+
+const MaterialBoxView = () => {
+  return (
+    <Canvas camera={{ position: [0, 0, 10], fov: 35 }}>
+      <MetalBox />
+    </Canvas>
+  );
+};
+
+export default MaterialBoxView;
